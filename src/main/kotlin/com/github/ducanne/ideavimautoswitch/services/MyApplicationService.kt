56cb4bb29bc896fce260f4c4782d2721ef4417c8
GitHub Action <action@github.com>
2022-02-18 21:26:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/ducanne/ideavimautoswitch/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ducanne/ideavimautoswitch/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ducanne.ideavimautoswitch.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ducanne.ideavimautoswitch.MyBundle
 
 class MyApplicationService {
 
